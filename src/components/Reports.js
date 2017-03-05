213aab78f1681929d9f12bebdff7a30f3dfb6715
Richard Farman <farmanrl@whitman.edu>
2017-03-05 12:39:49 -0800

    Charts for reports.

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -2,12 +2,18 @@ import React, { Component, PropTypes } from 'react';
 import {
   Accordion,
   Panel,
+  Modal,
+  ProgressBar,
+  ControlLabel,
+  Badge,
+  Button,
 } from 'react-bootstrap';
 import { addStyle } from 'react-bootstrap/lib/utils/bootstrapUtils';
+import Charts from './Charts';
 
 addStyle(Panel, 'dark');
 
-const reports = {
+const reportStyle = {
   position: 'absolute',
   maxWidth: 300,
   top: 130,
@@ -18,26 +24,82 @@ const reports = {
 class Reports extends Component {
   static propTypes = {
     filter: PropTypes.string,
-    total: PropTypes.number,
-    safe: PropTypes.number,
-    danger: PropTypes.number,
+    safe: PropTypes.object,
+    danger: PropTypes.object,
   }
 
   render() {
+    const reports = this.props.safe.size + this.props.danger.size;
+    const risk = this.props.danger.size / reports;
+    let rating = null;
+    if (risk <= 0.25) {
+      rating = 'Low';
+    } else if (risk <= 0.5) {
+      rating = 'Moderate';
+    } else if (risk <= 0.75) {
+      rating = 'High';
+    } else {
+      rating = 'Extreme';
+    }
     return (
-      <Accordion
-        defaultActiveKey="0"
-        style={reports}
-      >
-        <Panel
-          bsSize="sm"
-          bsStyle="dark"
-          header={<p>{this.props.filter} - {this.props.total} reports</p>}
+      <div>
+        <Accordion
+          defaultActiveKey="0"
+          style={reportStyle}
         >
-          <p>{this.props.safe} <strong>safe</strong></p>
-          <p>{this.props.danger} <strong>danger</strong></p>
-        </Panel>
-      </Accordion>
+          <Panel
+            bsSize="sm"
+            bsStyle="dark"
+            header={<p><strong>{this.props.filter}</strong> - {reports} reports</p>}
+          >
+            <p><strong>{this.props.safe.size}</strong> safe</p>
+            <p><strong>{this.props.danger.size}</strong> danger</p>
+            <p><strong>{rating}</strong> risk</p>
+            <Button
+              onClick={() => this.props.showModal('report')}
+            >
+              Charts
+            </Button>
+          </Panel>
+        </Accordion>
+        <Modal show={this.props.show} onHide={this.props.close}>
+
+          <Modal.Header style={{ background: '#5bc0de', color: 'white' }} closeButton>
+            <Modal.Title>All reports</Modal.Title>
+          </Modal.Header>
+          {reports ?
+           <Modal.Body>
+             <Modal.Title>{rating} risk</Modal.Title>
+             <hr />
+             <ControlLabel>
+               {this.props.filter} - Reports {' '}
+               <Badge>{reports}</Badge>
+             </ControlLabel>
+             <ProgressBar>
+               <ProgressBar
+                 now={this.props.safe.size}
+                 max={reports}
+                 label="safe"
+                 key={1}
+               />
+               <ProgressBar
+                 now={this.props.danger.size}
+                 max={reports}
+                 label="danger"
+                 bsStyle="danger"
+                 key={2}
+               />
+             </ProgressBar>
+             <Charts safe={this.props.safe} danger={this.props.danger} />
+           </Modal.Body>
+           :
+           <Modal.Body>
+             <Modal.Title>No active reports</Modal.Title>
+           </Modal.Body>
+          }
+
+        </Modal>
+      </div>
     );
   }
 }
